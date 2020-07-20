9bc1f9bb49e215cb35716cad8f1dc9ca81315f90
Aditya Firman Ihsan <aditya.fphoenix@gmail.com>
2020-07-20 13:41:21 +0700

    add more preprocessing functions

--- preprocess.py
+++ preprocess.py
@@ -9,7 +9,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import pandas as pd
 
-def get_input(data, test_split=0.2):
+def split_data(data, test_split=0.2):
     sentences = data['text'].tolist()
     labels = data['sentiment'].tolist()
 
@@ -18,13 +18,14 @@ def get_input(data, test_split=0.2):
 
     train_sentences = sentences[0:training_size]
     test_sentences = sentences[training_size:]
+        
     train_labels = np.array(labels[0:training_size])
     test_labels = np.array(labels[training_size:])
 
-    return (train_sentences, train_labels), (test_sentences, test_labels) 
+    return (train_sentences, test_sentences), (train_labels, test_labels) 
 
 def get_data(fname, source, raw=True):
-    path = tf.keras.utils.get_file(fname, url)
+    path = tf.keras.utils.get_file(fname, source)
     dataset = pd.read_csv(path)
     # print(dataset.head())
     if raw:
@@ -32,21 +33,35 @@ def get_data(fname, source, raw=True):
     else:
         return get_input(dataset)
 
-def to_sequence(vocab_source, 
-                target='same', 
+def tokenize(vocab_source, vocab_size=None):
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token="<OOV>")
+    tokenizer.fit_on_texts(vocab_source)
+    return tokenizer
+
+def to_sequence(target, tokenizer=None,
                 vocab_size=None, 
                 maxlen=None, 
                 focus='after'):
-    manip='pre' if focus=='after' else 'post'            
-    tokenizer = Tokenizer(num_words=vocab_size, oov_token="<OOV>")
-    tokenizer.fit_on_texts(vocab_source)
-    word_index = tokenizer.word_index
-    target = vocab_source if target=='same' else target
-    seq = tokenizer.texts_to_sequences(target)
-    seq = pad_sequences(seq, maxlen=None, padding=manip, truncating=manip)
-    return seq, word_index
+    manip='pre' if focus=='after' else 'post'
 
-def decode(text, reverse_dict):
-    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+    if tokenizer == None: 
+        tokenizer = tokenize(target, vocab_size=vocab_size)
+
+    seq = tokenizer.texts_to_sequences(target)
+    seq = pad_sequences(seq, maxlen=maxlen, padding=manip, truncating=manip)
+    return seq, tokenizer
+
+def get_input_sequences(train_sen, test_sen, **kwargs):
+    train_sequences, tokens = to_sequence(train_sen, **kwargs)
+    test_sequences, _ = to_sequence(test_sen, tokenizer=tokens, **kwargs)
+
+    return (train_sequences, test_sequences), tokens
+
+def decode(text, tokenizer):
+    dictionary = tokenizer.word_index
+    reverse_dict = ['']+list(dictionary.keys())
+    raw = [reverse_dict[i] for i in text]
+    clean_text = ' '.join(raw).strip()
+    return clean_text
  
 
